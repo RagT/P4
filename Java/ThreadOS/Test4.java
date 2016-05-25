937921d2b879e28a1644b51b07ac15db62300670
RagT <raghutir@gmail.com>
2016-05-25 14:53:04 -0700

    finished Test4

--- Java/ThreadOS/Test4.java
+++ Java/ThreadOS/Test4.java
@@ -1,4 +1,5 @@
 import java.util.Random;
+import java.util.Date;
 
 /**
  * Test4.java
@@ -9,6 +10,9 @@ public class Test4 extends Thread{
     private boolean enabled; //Is cache enabled?
     private int testType; //What test to run?
     private Random rand; //Used to run random tests
+    private byte[] read; //Read from this array
+    private byte[] write; //Write to this array
+    private int[] locations; //Used to store read/write locations for tests
 
     public Test4(String[] args) {
         if(args[0].equals("-enabled")) {
@@ -17,8 +21,13 @@ public class Test4 extends Thread{
         } else { //Don't use cache
             enabled = false;
         }
+        read = new byte[1024];
+        write = new byte[1024];
+        rand.nextBytes(write);      //Fill write block with random values
+
         testType = Integer.parseInt(args[1]);
         rand = new Random();
+        locations = new int[500];
     }
 
     //Runs test based on test type passed in command line arguments
@@ -38,29 +47,52 @@ public class Test4 extends Thread{
                 adversary();
                 break;
             default:
-                SysLib.cout("Invalid test type.");
+                SysLib.cerr("Invalid test type.\n");
                 break;
         }
+        sync();
+        SysLib.exit();
     }
 
     //read and write many blocks randomly across the disk.
     public void random() {
-
+        SysLib.cout("Random Access Test: \n");
+        for(int i = 0; i < locations.length; i++) {
+            locations[i] = rand.nextInt(1024);    //Fill locations array with random locations
+        }
+        testReadAndWrite("Random");
     }
 
     //90% of the total disk operations should be localized accesses and 10% should be random accesses.
     public void mixed() {
-
+        SysLib.cout("Mixed access test: \n");
+        for(int i = 0; i < locations.length; i++) {
+            int randInt = rand.nextInt(10); //Random number between 0 and 9
+            if(randInt < 9) { //localized access
+                locations[i] = rand.nextInt(15);
+            } else { //Completely random access
+                locations[i] = rand.nextInt(1024);
+            }
+        }
+        testReadAndWrite("Mixed");
     }
 
     //generate disk accesses that do not make good use of the disk cache at all.
     public void adversary() {
-
+        SysLib.cout("Localized Access Test: \n");
+        for(int i = 0; i < locations.length; i++){
+            locations[i] = i;
+        }
+        testReadAndWrite("Adversary");
     }
 
     //read and write a small selection of blocks many times to get a high ratio of cache hits.
     public void localized() {
-
+        SysLib.cout("Localized Access Test: \n");
+        for(int i = 0; i < locations.length; i++){
+            locations[i] = rand.nextInt(15);
+        }
+        testReadAndWrite("Localized");
     }
 
     //Will use csync() if cache enabled will use sync() if not
@@ -98,5 +130,21 @@ public class Test4 extends Thread{
 
             SysLib.rawwrite(bId, buffer);
         }
+    }
+
+    public void testReadAndWrite(String testType) {
+        long startRead = new Date().getTime();
+        for(int i = 0; i < locations.length; i++) {
+            read(locations[i], read);
+        }
+        long readElapsed = new Date().getTime() - startRead;
+
+        SysLib.cout(testType + " read elapsed time = " + readElapsed + "ms.\n");
+        long startWrite = new Date().getTime();
+        for(int i = 0; i < locations.length; i++) {
+            write(locations[i], write);
+        }
+        long writeElapsed = new Date().getTime() - startWrite;
+        SysLib.cout(testType + " write elapsed time = " + writeElapsed + "ms.\n");
     }
 }
